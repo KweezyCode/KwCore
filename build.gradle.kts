0d1d60dbc0dcf47ece9a98f1f1362401e7f7a80d
Kweezy <unknown2w+github@gmail.com>
2024-08-31 19:00:00 +0300

    Обновление build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly(project(":craftbukkit"))
+    implementation(project(":craftbukkit"))
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
